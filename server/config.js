bfe427e0c7df7eee2c8957adbec7f0a5d5377b24
it21310546 <it21310546@my.sliit.lk>
2023-05-14 19:20:45 +0530

    first commit

---
+++ server/config.js
@@ -0,0 +1,7 @@
+export default {
+    JWT_SECRET: "TXNQzh7dckQ/ Kb7wXhw21aYI / cHd7y7VRezal.Hew0Qs=",
+    EMAIL: "lulu.mertz@ethereal.email",
+    PASSWORD: "qngYSjvtSTP8NwUpVH",
+    ATLAS_URI: "mongodb+srv://nuwandi:nuwandi123@cluster0.doxob8q.mongodb.net/?retryWrites=true&w=majority"
+
+}
